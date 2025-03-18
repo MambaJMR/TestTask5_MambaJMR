103fcbf56a09b670e8bef478208f5b7c50c91721
krabo <Krabovich05@gmail.com>
2025-03-19 00:36:27 +0300

    Добавьте файлы проекта.

---
+++ Services/Implementations/SessionService.cs
@@ -0,0 +1,35 @@
+﻿using Lab5TestTask.Data;
+using Lab5TestTask.Models;
+using Lab5TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5TestTask.Services.Implementations;
+
+/// <summary>
+/// SessionService implementation.
+/// Implement methods here.
+/// </summary>
+public class SessionService : ISessionService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public SessionService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Session> GetSessionAsync()
+    {
+        return await _dbContext.Sessions
+            .Where(s => s.DeviceType == Enums.DeviceType.Desktop)
+            .OrderBy(s => s.StartedAtUTC)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Session>> GetSessionsAsync()
+    {
+        return await _dbContext.Sessions
+            .Where(s => s.User.Status == Enums.UserStatus.Active && s.EndedAtUTC.Year < 2025)
+            .ToListAsync();
+    }
+}
